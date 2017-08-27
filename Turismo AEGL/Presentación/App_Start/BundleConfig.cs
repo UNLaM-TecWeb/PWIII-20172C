3662bf29d56c60f293125a8ae2c7a71d691259d5
gerAlvarez <agro.075@gmail.com>
2017-08-27 12:15:56 -0300

    Primer commit del proyecto.
    + Se creó la capa de presentación de la aplicación. La llamé Presentación.
    + Se creó el directorio Content dónde se guardan las aplicaciones de terceros (por ejemplo: bootstrap, jquery, etc).
    + Se agregaron al directorio Content las aplicaciones jquery 3.2.1 y bootstrap 3.3.7.
    + Se agregó al proyecto la dependencia System.Web.Optimization para poder trabajar con los bundles. Esto es un requisito técnico del trabajo practico (punto 3.3.3).

---
+++ Turismo AEGL/Presentación/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace Presentación.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Script bundles
+            bundles.Add(new ScriptBundle("~/Bundles/scripts").Include("~/Content/jquery-3.2.1/jquery-3.2.1.*"));
+            bundles.Add(new ScriptBundle("~/Bundles/scripts").Include("~/Content/bootstrap-3.3.7/js/*.js"));
+
+            // Style bundles
+            bundles.Add(new StyleBundle("~/Bundles/styles").Include("~/Content/bootstrap-3.3.7/css/*"));
+            
+            // Habilitamos la inteligencia para que durante la sesión de debug se utilicen los archivos de desarrollo en lugar de los de producción, la minificación, etc.
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ Turismo AEGL/Presentación/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using Presentación.App_Start;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Presentación
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
