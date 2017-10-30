e2910261f983764b0628bf87716ac1c17f210e75
Guinovart, Jose A <jose.a.guinovart@accenture.com>
2017-10-30 14:36:50 -0300

    Cambios en el route y nuevos controladores

---
+++ Turismo AEGL/Logica/Models/LogicaPaquete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dal;
+
+namespace Logica.Models
+{
+    class LogicaPaquete
+    {
+        public Paquete AgregarPaquete(Paquete p)
+        {
+            TurismoAEGLContext Contexto = new TurismoAEGLContext();
+            Paquete paquete = new Paquete();
+
+            paquete = p;
+
+        }
+    }
+}
--- Turismo AEGL/Presentación/App_Start/RouteConfig.cs
+++ Turismo AEGL/Presentación/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Presentación
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "AdminPaquete",
+                url: "admin/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
